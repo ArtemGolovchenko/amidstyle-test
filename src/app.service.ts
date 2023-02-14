775f53d5aa93031fefc92d49153d8d00e23dc9a6
Artem Holovchenko <art2937@gmail.com>
2023-02-14 21:46:31 +0200

    fix(block-data): fix bad request error

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@nestjs/common';
-const pt = require('puppeteer');
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+const { executablePath } = require("puppeteer");
 
 @Injectable()
 export class AppService {
   async getResponse(url) {
     try {
-      const browser = await pt.launch()
-      const page = await browser.newPage()
-      await page.goto(url.url, {
-        waitUntil: 'networkidle0',
-      })
-      const pageData = await page.$("#data")
-      const text = await (await pageData.getProperty('textContent')).jsonValue()
-      await browser.close();
-
-      return text;
+      let blockData: string;
+      puppeteer.use(pluginStealth());
+      await puppeteer.launch({ executablePath: executablePath() }).then(async browser => {
+        const page = await browser.newPage();
+        await page.goto(url.url, {
+          waitUntil: 'networkidle0'
+        });
+        const pageData = await page.$("#data")
+        blockData = await (await pageData.getProperty('textContent')).jsonValue()
+        await browser.close();
+      });
+      return blockData;
     }
     catch (e) {
       return e.message;
